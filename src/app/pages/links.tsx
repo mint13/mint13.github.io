bcadd39a8c1b500a086b5cbe11561f9d8ac14321
Mint Medillo <m.medillo13@gmail.com>
2024-03-25 15:56:34 +0100

    add instagram to links

--- src/app/pages/links.tsx
+++ src/app/pages/links.tsx
@@ -3,29 +3,36 @@ import {
   FaLinkedin,
   FaEnvelope,
   FaRegIdCard,
+  FaInstagram,
 } from "react-icons/fa";
 
 export const links = [
   {
     index: 0,
-    title: "Github Profile",
+    title: "Github",
     href: "https://github.com/mint13",
     icon: <FaGithub />,
   },
   {
     index: 1,
-    title: "Find me on LinkedIn",
+    title: "LinkedIn",
     href: "https://www.linkedin.com/in/mint-medillo/",
     icon: <FaLinkedin />,
   },
   {
     index: 2,
+    title: "Instagram",
+    href: "https://www.instagram.com/_mintm13/",
+    icon: <FaInstagram  />,
+  },
+  {
+    index: 3,
     title: "Get in Touch",
     href: "mailto:m.medillo13@gmail.com",
     icon: <FaEnvelope />,
   },
   {
-    index: 3,
+    index: 4,
     title: "Curriculum Vitae",
     href: "https://drive.google.com/file/d/1GZGAkCoefk9JHqW4yBQJh_ClMI1L94X0/view",
     icon: <FaRegIdCard />,
