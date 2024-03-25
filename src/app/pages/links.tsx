33aabbee8751e8a38fbbb38fe7580046860869da
Mint Medillo <m.medillo13@gmail.com>
2024-03-25 15:26:18 +0100

    add src directory

---
+++ src/app/pages/links.tsx
@@ -0,0 +1,33 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaRegIdCard,
+} from "react-icons/fa";
+
+export const links = [
+  {
+    index: 0,
+    title: "Github Profile",
+    href: "https://github.com/mint13",
+    icon: <FaGithub />,
+  },
+  {
+    index: 1,
+    title: "Find me on LinkedIn",
+    href: "https://www.linkedin.com/in/mint-medillo/",
+    icon: <FaLinkedin />,
+  },
+  {
+    index: 2,
+    title: "Get in Touch",
+    href: "mailto:m.medillo13@gmail.com",
+    icon: <FaEnvelope />,
+  },
+  {
+    index: 3,
+    title: "Curriculum Vitae",
+    href: "https://drive.google.com/file/d/1GZGAkCoefk9JHqW4yBQJh_ClMI1L94X0/view",
+    icon: <FaRegIdCard />,
+  },
+];
---
+++ src/app/pages/pages.ts
@@ -0,0 +1,9 @@
+export const pages = [
+  { index: 0, name: 'overview.md', route: '/overview', visible: true },
+  { index: 1, name: 'skills.md', route: '/skills', visible: true },
+  { index: 2, name: 'experience.md', route: '/experience', visible: true },
+  { index: 3, name: 'projects.md', route: '/projects', visible: true },
+  { index: 4, name: 'community.md', route: '/community', visible: true },  
+  { index: 5, name: 'contact.md', route: '/certificates', visible: true },
+  { index: 6, name: 'docs.md', route: '/docs', visible: false },  
+];
